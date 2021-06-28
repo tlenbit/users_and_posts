917b368333b12a598cc6b035dd15d446cfade5a2
markov <i.markov@group-ib.com>
2021-06-28 18:16:47 +0300

    add user views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+from .serializers import UserSerializer
+from .models import User
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
--- users_and_posts/urls.py
+++ users_and_posts/urls.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
+from rest_framework import routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+from users.views import UserViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
